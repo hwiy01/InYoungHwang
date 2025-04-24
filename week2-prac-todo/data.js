04d1d06a7658abfb95cd0765a9f170129800fb04
hwiy01 <vdocom02@naver.com>
2025-04-24 21:54:54 +0900

    add : todo 초기 데이터 추가

---
+++ week2-prac-todo/data.js
@@ -0,0 +1,50 @@
+export const todos = [
+    {
+      id: 1,
+      title: '과제하기',
+      completed: false,
+      priority: 1,
+    },
+    {
+      id: 2,
+      title: '밥 먹기',
+      completed: false,
+      priority: 2,
+    },
+    {
+      id: 3,
+      title: '스터디 준비하기',
+      completed: true,
+      priority: 1,
+    },
+    {
+      id: 4,
+      title: '세미나 듣기',
+      completed: false,
+      priority: 3,
+    },
+    {
+      id: 5,
+      title: '운동 가기',
+      completed: false,
+      priority: 2,
+    },
+    {
+      id: 6,
+      title: '방 청소하기',
+      completed: true,
+      priority: 2,
+    },
+    {
+      id: 7,
+      title: '책 읽기',
+      completed: false,
+      priority: 1,
+    },
+    {
+      id: 8,
+      title: '행복하기',
+      completed: false,
+      priority: 3,
+    },
+  ];
---
+++ week2-prac-todo/index.js
@@ -0,0 +1,35 @@
+// 요소 선택 !!!
+const input = document.querySelector('.todo-input');
+const addBtn = document.querySelector('.add-btn');
+const todoList = document.querySelector('.todo-list');
+
+// 로컬스토리지에서 가져오기
+let todos = JSON.parse(localStorage.getItem('todos') || '[]');
+
+// 초기화 - 화면에 표시
+todos.forEach((todo) => {
+    const li = document.createElement('li');
+    li.textContent = todo;
+    todoList.appendChild(li);
+});
+
+
+// 추가 버튼 클릭 이벤트
+addBtn.addEventListener('click', (e) => {
+    const value = input.value;
+
+    if(!value){
+        return;
+    }
+  // 리스트에 추가
+    const li = document.createElement('li');
+    li.textContent = value;
+    todoList.appendChild(li);
+
+  // 로컬스토리지에 저장
+    todos.push(value);
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+    // input 값을 초기화
+    input.value = '';
+});
